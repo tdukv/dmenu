8849c9b9a89bebba3ee7cf79b858773ecc5e03a7
tdu <tdukv@protonmail.com>
2020-06-04 17:51:44 +0300

    fix -b option

--- dmenu.c
+++ dmenu.c
@@ -959,8 +959,10 @@ main(int argc, char *argv[])
 		if (!strcmp(argv[i], "-v")) {      /* prints version information */
 			puts("dmenu-"VERSION);
 			exit(0);
-		} else if (!strcmp(argv[i], "-b")) /* appears at the bottom of the screen */
+		} else if (!strcmp(argv[i], "-b")) { /* appears at the bottom of the screen */
 			topbar = 0;
+			centered = 0;
+		}
 		else if (!strcmp(argv[i], "-f"))   /* grabs keyboard before reading stdin */
 			fast = 1;
 		else if (!strcmp(argv[i], "-t"))   /* on top */
